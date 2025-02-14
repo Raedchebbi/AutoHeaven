d96fb229f6dfe527a43152c74f013e49949f0027
kalthum01 <kalthoumsakka@yahoo.com>
2025-02-14 18:33:54 +0100

    Crud offre implémenté

---
+++ src/main/java/models/Offre.java
@@ -0,0 +1,89 @@
+package models;
+
+public class Offre {
+    private int id_offre;
+    private String type_offre;
+    private String description;
+    private double taux_reduction;
+    private String date_debut;
+    private String date_fin;
+    private int id_equipement;
+
+    // Default constructor
+    public Offre() {}
+
+    // Parameterized constructor
+    public Offre(int id_offre, String type_offre, String description, double taux_reduction, String date_debut, String date_fin, int id_equipement) {
+        this.id_offre = id_offre;
+        this.type_offre = type_offre;
+        this.description = description;
+        this.taux_reduction = taux_reduction;
+        this.date_debut = date_debut;
+        this.date_fin = date_fin;
+        this.id_equipement = id_equipement;
+    }
+
+    // Getters and Setters
+    public int getId_offre() {
+        return id_offre;
+    }
+    public void setId_offre(int id_offre) {
+        this.id_offre = id_offre;
+    }
+
+    public String getType_offre() {
+        return type_offre;
+    }
+    public void setType_offre(String type_offre) {
+        this.type_offre = type_offre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTaux_reduction() {
+        return taux_reduction;
+    }
+    public void setTaux_reduction(double taux_reduction) {
+        this.taux_reduction = taux_reduction;
+    }
+
+    public String getDate_debut() {
+        return date_debut;
+    }
+    public void setDate_debut(String date_debut) {
+        this.date_debut = date_debut;
+    }
+
+    public String getDate_fin() {
+        return date_fin;
+    }
+    public void setDate_fin(String date_fin) {
+        this.date_fin = date_fin;
+    }
+
+    public int getId_equipement() {
+        return id_equipement;
+    }
+    public void setId_equipement(int id_equipement) {
+        this.id_equipement = id_equipement;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Offre{" +
+                "id_offre=" + id_offre +
+                ", type_offre='" + type_offre + '\'' +
+                ", description='" + description + '\'' +
+                ", taux_reduction=" + taux_reduction +
+                ", date_debut='" + date_debut + '\'' +
+                ", date_fin='" + date_fin + '\'' +
+                ", id_equipement=" + id_equipement +
+                '}';
+    }
+}
---
+++ src/main/java/services/OffreService.java
@@ -0,0 +1,93 @@
+package services;
+
+import models.Offre;
+import utils.MyDb;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OffreService implements Crud<Offre> {
+    Connection conn;
+
+    public OffreService() {
+        this.conn = MyDb.getInstance().getConn();
+    }
+
+    @Override
+    public void create(Offre offre) throws Exception {
+        String sql = "INSERT INTO offre (type_offre, description, taux_reduction, date_debut, date_fin, id_equipement) VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, offre.getType_offre());
+        stmt.setString(2, offre.getDescription());
+        stmt.setDouble(3, offre.getTaux_reduction());
+        stmt.setString(4, offre.getDate_debut());
+        stmt.setString(5, offre.getDate_fin());
+        stmt.setInt(6, offre.getId_equipement());
+        stmt.executeUpdate();
+
+        ResultSet rs = stmt.getGeneratedKeys();
+        if (rs.next()) {
+            offre.setId_offre(rs.getInt(1));
+        }
+
+        System.out.println("Offre ajoutée avec succès !");
+    }
+
+    @Override
+    public void update(Offre offre) throws Exception {
+        String sql = "UPDATE offre SET type_offre = ?, description = ?, taux_reduction = ?, date_debut = ?, date_fin = ?, id_equipement = ? WHERE id_offre = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, offre.getType_offre());
+        stmt.setString(2, offre.getDescription());
+        stmt.setDouble(3, offre.getTaux_reduction());
+        stmt.setString(4, offre.getDate_debut());
+        stmt.setString(5, offre.getDate_fin());
+        stmt.setInt(6, offre.getId_equipement());
+        stmt.setInt(7, offre.getId_offre());
+
+        int rowsUpdated = stmt.executeUpdate();
+        if (rowsUpdated > 0) {
+            System.out.println("Offre mise à jour avec succès !");
+        } else {
+            throw new Exception("Offre non trouvée.");
+        }
+    }
+
+    @Override
+    public void delete(Offre offre) throws Exception {
+        String sql = "DELETE FROM offre WHERE id_offre = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, offre.getId_offre());
+
+        int rowsDeleted = stmt.executeUpdate();
+        if (rowsDeleted > 0) {
+            System.out.println("Offre supprimée avec succès !");
+        } else {
+            throw new Exception("Offre non trouvée.");
+        }
+    }
+
+    @Override
+    public List<Offre> getAll() throws Exception {
+        String sql = "SELECT * FROM offre";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        List<Offre> offres = new ArrayList<>();
+
+        while (rs.next()) {
+            Offre offre = new Offre();
+            offre.setId_offre(rs.getInt("id_offre"));
+            offre.setType_offre(rs.getString("type_offre"));
+            offre.setDescription(rs.getString("description"));
+            offre.setTaux_reduction(rs.getDouble("taux_reduction"));
+            offre.setDate_debut(rs.getString("date_debut"));
+            offre.setDate_fin(rs.getString("date_fin"));
+            offre.setId_equipement(rs.getInt("id_equipement"));
+            offres.add(offre);
+        }
+        return offres;
+    }
+}
