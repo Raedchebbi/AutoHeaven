cb841a9f38968d8fe5c47825c12d211f185b10fb
tasnim3A21 <Tasnim.OUESLATI@esprit.tn>
2025-02-17 19:59:24 +0100

    implementation de l'ajout d'un equipement au panier

--- src/main/java/controllers/DetailEquipement.java
+++ src/main/java/controllers/DetailEquipement.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -8,6 +12,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import models.EquipementAffichage;
+import models.Panier;
+import services.EquipementService;
+import services.PanierService;
+
+import java.io.IOException;
 
 
 public class DetailEquipement {
@@ -54,8 +63,60 @@ public class DetailEquipement {
         image.setImage(new Image(equipement.getImage()));
         disponibilite.setText(equipement.getQuantite() > 0 ? "In Stock" : "Out of Stock");
         desc.setText(equipement.getDescription());
+        input.setText(String.valueOf(1));
+
 
 
 
     }
+    @FXML
+
+    private void handleAddAction() throws Exception {try {
+        int value = Integer.parseInt(input.getText());
+        input.setText(String.valueOf(value + 1));
+    } catch (NumberFormatException e) {
+        System.err.println("Erreur : la valeur du champ n'est pas un entier valide.");
+    }
+
+       // PanierService es = new PanierService();
+
+
+
+    }
+    @FXML
+    private void handleSousAction() throws Exception {try {
+        int value = Integer.parseInt(input.getText());
+        if (value > 0) {
+        input.setText(String.valueOf(value -1));}
+    } catch (NumberFormatException e) {
+        System.err.println("Erreur : la valeur du champ n'est pas un entier valide.");
+    }}
+    @FXML
+    private void handleAddToCart() throws Exception {
+        int id =equipement.getId();
+        int idu =3;
+        PanierService ps = new PanierService();
+        Panier panier = new Panier(Integer.parseInt(input.getText()),idu,id);
+        ps.create(panier);
+        showSuccessPopup();
+        redirectToEquipDetail();
+
+    }
+    private void showSuccessPopup() throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Succès");
+        alert.setHeaderText(null);
+        alert.setContentText("Votre produit a été ajouté avec succès au panier!");
+        alert.showAndWait();
+       // redirectToEquipDetail();
+    }
+
+    private void redirectToEquipDetail() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Detail_equipement.fxml"));
+        Parent root = loader.load();
+        Scene scene = cart.getScene();
+        scene.setRoot(root);
+    }
+
+
 }
