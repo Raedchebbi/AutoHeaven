e4a8508994016351eb678570281b647b04a9b11a
Rayen <rayen.elfil@esprit.tn>
2025-02-17 03:14:43 +0100

    CRUD remorquage

--- src/main/java/models/Remorquage.java
+++ src/main/java/models/Remorquage.java
@@ -1,4 +1,124 @@
 package models;
+import java.util.Date;
 
 public class Remorquage {
+    private int id_rm;
+    private User user;
+    //private Voiture voiture;
+    private int id_v;
+    private Date date_demande;
+    private String adresse;
+    private String destination;
+    private String statut;
+    private double prix;
+    private Date date_fin;
+
+    public Remorquage() {}
+
+    public Remorquage(int id_rm, User user, int id_v /*Voiture voiture*/, Date date_demande, String adresse, String destination, String statut, double prix, Date date_fin) {
+        this.id_rm = id_rm;
+        this.user = user;
+        //this.voiture = voiture;
+        this.id_v = id_v;
+        this.date_demande = date_demande;
+        this.adresse = adresse;
+        this.destination = destination;
+        this.statut = statut;
+        this.prix = prix;
+        this.date_fin = date_fin;
+    }
+
+    public int getId_rm() {
+        return id_rm;
+    }
+
+    public void setId_rm(int id_rm) {
+        this.id_rm = id_rm;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /*public Voiture getVoiture() {
+        return voiture;
+    }
+    public void setVoiture(Voiture voiture) {
+        this.voiture = voiture;
+    }*/
+
+    public int getId_v() {
+        return id_v;
+    }
+
+    public void setId_v(int id_v) {
+        this.id_v = id_v;
+    }
+
+    public Date getDate_demande() {
+        return date_demande;
+    }
+
+    public void setDate_demande(Date date_demande) {
+        this.date_demande = date_demande;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public Date getDate_fin() {
+        return date_fin;
+    }
+
+    public void setDate_fin(Date date_fin) {
+        this.date_fin = date_fin;
+    }
+
+    @Override
+    public String toString() {
+        return "Remorquage{" +
+                "id_rm=" + id_rm +
+                ", user_id=" + (user != null ? user.getId() : "null") +
+                //", voiture_id=" + (voiture != null ? voiture.getId_v() : "null") +
+                ", id_v=" + id_v +
+                ", date_demande=" + date_demande +
+                ", adresse='" + adresse + '\'' +
+                ", destination='" + destination + '\'' +
+                ", statut='" + statut + '\'' +
+                ", prix=" + prix +
+                ", date_fin=" + date_fin +
+                '}';
+    }
 }
--- src/main/java/services/RemorquageService.java
+++ src/main/java/services/RemorquageService.java
@@ -1,4 +1,114 @@
 package services;
 
-public class RemorquageService {
+import models.Remorquage;
+import utils.MyDb;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemorquageService implements Crud<Remorquage> {
+    private final Connection conn;
+
+    public RemorquageService() {
+        this.conn = MyDb.getInstance().getConnection();
+    }
+
+    @Override
+    public void create(Remorquage obj) throws Exception {
+        String sql = "INSERT INTO remorquage (id, id_v, date_demande, adresse, destination, statut, prix) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, obj.getUser().getId());
+            //stmt.setInt(2, obj.getVoiture().getId_v());
+            stmt.setInt(2, obj.getId_v());
+            stmt.setTimestamp(3, new Timestamp(obj.getDate_demande().getTime()));
+            stmt.setString(4, obj.getAdresse());
+            stmt.setString(5, obj.getDestination());
+            stmt.setString(6, "en_attente");
+            stmt.setDouble(7, obj.getPrix());
+
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted > 0) {
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        obj.setId_rm(generatedKeys.getInt(1));
+                        System.out.println("Demande de remorquage ajoutée avec ID : " + obj.getId_rm());
+                    }
+                }
+            } else {
+                throw new Exception("Échec de l'insertion de la demande de remorquage.");
+            }
+        }
+    }
+
+    @Override
+    public void update(Remorquage obj) throws Exception {
+        String sql = "UPDATE remorquage SET statut = ?, date_fin = ? WHERE id_rm = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, obj.getStatut());
+            stmt.setTimestamp(2, new Timestamp(obj.getDate_fin().getTime()));
+            stmt.setInt(3, obj.getId_rm());
+            stmt.executeUpdate();
+        }
+    }
+
+    @Override
+    public void delete(Remorquage obj) throws Exception {
+        String sql = "DELETE FROM remorquage WHERE id_rm = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, obj.getId_rm());
+            stmt.executeUpdate();
+        }
+    }
+
+    @Override
+    public List<Remorquage> getAll() throws Exception {
+        List<Remorquage> remorquages = new ArrayList<>();
+        String sql = "SELECT * FROM remorquage";
+        try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Remorquage remorquage = new Remorquage();
+                remorquage.setId_rm(rs.getInt("id_rm"));
+                remorquage.setUser(UserService.getById(rs.getInt("id")));
+                //remorquage.setVoiture(voitureService.getById(rs.getInt("id_v")));
+                remorquage.setId_v(rs.getInt("id_v"));
+                remorquage.setDate_demande(rs.getTimestamp("date_demande"));
+                remorquage.setAdresse(rs.getString("adresse"));
+                remorquage.setDestination(rs.getString("destination"));
+                remorquage.setStatut(rs.getString("statut"));
+                remorquage.setPrix(rs.getDouble("prix"));
+                remorquage.setDate_fin(rs.getTimestamp("date_fin"));
+
+                remorquages.add(remorquage);
+            }
+        }
+        return remorquages;
+    }
+
+    public Remorquage getById(int id_rm) throws Exception {
+        String sql = "SELECT * FROM remorquage WHERE id_rm = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id_rm);
+            ResultSet rs = stmt.executeQuery();
+            UserService userService = new UserService();
+            //VoitureService voitureService = new VoitureService();
+
+            if (rs.next()) {
+                Remorquage remorquage = new Remorquage();
+                remorquage.setId_rm(rs.getInt("id_rm"));
+                userService.getById(rs.getInt("id"));
+                remorquage.setId_v(rs.getInt("id_v"));
+                //voitureService.getById(rs.getInt("id_v")),
+                remorquage.setDate_demande(rs.getTimestamp("date_demande"));
+                remorquage.setAdresse(rs.getString("adresse"));
+                remorquage.setDestination(rs.getString("destination"));
+                remorquage.setStatut(rs.getString("statut"));
+                remorquage.setPrix(rs.getDouble("prix"));
+                remorquage.setDate_fin(rs.getTimestamp("date_fin"));
+
+                return remorquage;
+            }
+        }
+        return null;
+    }
 }
